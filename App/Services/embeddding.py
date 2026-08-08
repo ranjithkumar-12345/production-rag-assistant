3251e94fe4689fa2a90ab6a9a86a06cd3c32a405
ranjith <ranjithkumar180205@gmail.com>
2026-08-08 17:58:35 +0530

    add a function embedding_text

---
+++ App/Services/embeddding.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import google.genai as genai
+
+load_dotenv()
+
+client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+def embedding_text(text:str):
+
+    result = client.models.embed_content(
+        model = "gemini-embedding-001",
+        contents = text 
+    )
+    return result.embeddings[0].values
+
+
+if __name__ =="__main__":
+
+    embedding = embedding_text("My name is ranjith")
+    print(len(embedding))
+    print(embedding[:6])
