4457bcb9261487a63eb6f7203ad35c5df665db26
ranjith <ranjithkumar180205@gmail.com>
2026-08-10 12:10:19 +0530

    create a class llm and intialize llm add a function generate

---
+++ App/Services/llm.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import google.genai as genai
+
+load_dotenv()
+
+class llm():
+    def __init__(self):
+
+        self.client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+
+        self.model = "gemini-1.5-flash"
+
+    def generate(self,context:str,questio:str,)->str:
+        prompt = f"context =context\n\nQuestion=question\n\nAnswer:"
+        response = self.client.models.generate_content(
+            models=self.model,
+            content =prompt
+        )
+
+        return response.text
+
+if __name__ =="__main__":
+
+    LLm = llm()
+    print("LLm intialized successfully")
